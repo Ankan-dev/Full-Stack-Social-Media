4d19c730360f645fc9f3c8e65b2ad3234d53f420
Ankan Mandal <ankanmandal2001@gmail.com>
2024-12-23 23:26:48 +0530

    User controller is done

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require('../models/user-model.js');
-const { hashPassword, checkPassword} = require('../utils/hashing.js');
+const { hashPassword, checkPassword } = require('../utils/hashing.js');
 const sendEmail = require('../utils/sendEmail.js');
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 const registerUser = async (req, res) => {
     const { fullname, email, password, gender } = req.body;
@@ -121,60 +121,156 @@ const verifyUser = async (req, res) => {
             })
     } catch (error) {
         return res.status(500)
-                .json({
-                    message:"Internal server error in verifying the user",
-                    success:false
-                })
-    }
-
-
-}
-
-const userLogin=async(req,res)=>{
-    const {email,password}=req.body;
-
-    if(!email || !password){
-        return res.status(404)
-                .json({
-                    message:"Credentials are missing",
-                    success:false
-                })
-    }
-
-    const getUser=await User.findOne({Email:email});
-
-    if(!getUser){
-        return res.status(404)
-                .json({
-                    message:"No user found with this email id",
-                    success:false
-                })
-    }
-    
-    const checkUserPassword=await checkPassword(getUser.Password,password);
-
-    if(!checkUserPassword){
-        return res.status(404)
-                .json({
-                    message:"Incorrect Password",
-                    success:false
-                })
-    }
-
-    const token=jwt({id:getUser._id},process.env.JWT_SECRET,);
-
-    const options={
-        httpOnly:true,
-        secure:true
-    }
-
-    return res.status(200)
-            .cookie("Token",token,options)
             .json({
-                message:"THe user is loggedin Successfully",
-                success:true
+                message: "Internal server error in verifying the user",
+                success: false
             })
+    }
+
 
 }
 
-module.exports = { registerUser,verifyUser,userLogin };
+const userLogin = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(404)
+            .json({
+                message: "Credentials are missing",
+                success: false
+            })
+    }
+
+    try {
+        const getUser = await User.findOne({ Email: email });
+
+        if (!getUser) {
+            return res.status(404)
+                .json({
+                    message: "No user found with this email id",
+                    success: false
+                })
+        }
+
+        const checkUserPassword = await checkPassword(getUser.Password, password);
+
+        if (!checkUserPassword) {
+            return res.status(404)
+                .json({
+                    message: "Incorrect Password",
+                    success: false
+                })
+        }
+
+        const token = jwt({ id: getUser._id }, process.env.JWT_SECRET,);
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        return res.status(200)
+            .cookie("Token", token, options)
+            .json({
+                message: "THe user is loggedin Successfully",
+                success: true
+            })
+    } catch (error) {
+
+        res.status(500)
+            .json({
+                message: "Internal server error in user login",
+                error: error,
+                success: false
+            })
+    }
+
+
+
+}
+
+const userLogout = async (req, res) => {
+    const userId = req.user;
+
+    if (!userId) {
+        return res.status(404)
+            .json({
+                message: "user id is missing",
+                success: false
+            })
+    }
+
+    try {
+        const updateUser = await User.findOneAndUpdate({ _id: userId }, { RefreshToken: null }, { new: true });
+
+        if (!updateUser) {
+            return res.status(500)
+                .json({
+                    message: "Refresh Token can't be updated due to internal server error",
+                    success: false
+                })
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        res.status(200)
+            .clearCookie('Token', options)
+            .json({
+                message: "User is loggedout successfully",
+                success: false
+            })
+    } catch (error) {
+        return res.status(500)
+            .json({
+                message: "Internal server error in logging out",
+                error: error,
+                success: false
+            })
+    }
+
+
+}
+
+const userProfile = async (req, res) => {
+    const { userId } = req.params;
+
+    if (!userId) {
+        return res.status(404)
+            .json({
+                message: "User Id is missing",
+                seccess: false
+            })
+    }
+
+    try {
+        const getUser = await User.findById(userId);
+
+        if (!getUser) {
+            return res.status(404)
+                .json({
+                    message: "User not found",
+                    success: false
+                })
+        }
+
+        return res.status(200)
+            .json({
+                message: "User details fetched successfully",
+                data: getUser,
+                success: true
+            })
+    } catch (error) {
+        return res.status(500)
+                .json({
+                    message:"Internal server error in getting user profile",
+                    success:false
+                })
+    }
+
+
+}
+
+module.exports = { registerUser, verifyUser, userLogin, userLogout,userProfile };
--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -1,7 +1,12 @@
 const express=require('express');
 const Router=express.Router();
-const {registerUser}=require('../controllers/userController');
+const {registerUser,verifyUser, userLogin, userLogout,userProfile}=require('../controllers/userController');
+const authenticateUser=require('../middlewares/auth.js')
 
 Router.post('/register-user',registerUser);
+Router.post('/verify-user',verifyUser);
+Router.post('/login-user',userLogin);
+Router.post('/user-logout',authenticateUser,userLogout);
+Router.get('/profile-user',userProfile);
 
 module.exports=Router;
